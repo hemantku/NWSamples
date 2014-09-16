8aaf79ff612acf42826e1bf2eb7fadcb62d7020c
hemantku <vgh4uster@gmail.com>
2014-09-16 01:48:38 -0700

    Programs update

---
+++ echo-client.cc
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <iostream>
+
+#include "inet-client.h"
+
+using namespace std;
+
+int
+main(int argc, char **argv)
+{
+    int option;
+
+    // setup default arguments
+    int port = 3000;
+    string host = "localhost";
+
+    // process command line options using getopt()
+    // see "man 3 getopt"
+    while ((option = getopt(argc,argv,"h:p:")) != -1) {
+        switch (option) {
+            case 'p':
+                port = atoi(optarg);
+                break;
+            case 'h':
+                host = optarg;
+                break;
+            default:
+                cout << "client [-h host] [-p port]" << endl;
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    InetClient client = InetClient(host, port);
+    client.run();
+}
+
---
+++ echo-server.cc
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <iostream>
+
+#include "inet-server.h"
+
+using namespace std;
+
+int
+main(int argc, char **argv)
+{
+    int option, port;
+
+    // setup default arguments
+    port = 3000;
+
+    // process command line options using getopt()
+    // see "man 3 getopt"
+    while ((option = getopt(argc,argv,"p:")) != -1) {
+        switch (option) {
+            case 'p':
+                port = atoi(optarg);
+                break;
+            default:
+                cout << "server [-p port]" << endl;
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    InetServer server = InetServer(port);
+    server.run();
+}
